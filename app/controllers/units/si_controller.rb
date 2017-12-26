661b0960fc5f2f434e00c15dfe8ba33ad4828300
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-12-26 15:40:10 -0800

    Implement the case of missing unit params or empty, Added Test

--- app/controllers/units/si_controller.rb
+++ app/controllers/units/si_controller.rb
@@ -1,5 +1,14 @@
 class Units::SiController < ApplicationController
   def index
-    render json: { text: "hello" }
+    if !params[:units] || params[:units].length == 0
+      render json: { error: "Units params is missing or empty" }
+    else
+      render json: { value: units_params }
+    end
+  end
+
+  private
+  def units_params
+    params.require(:units)
   end
 end
--- spec/controllers/units/si_controller_spec.rb
+++ spec/controllers/units/si_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Units::SiController, type: :controller do
 
+  describe "Get #index" do
+    it "return a success http status code of 200" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    context "when given no units params" do
+      it "return an object with Missing params units error" do
+        get :index
+        json_response = JSON.parse(response.body)
+
+        expect(json_response["error"]).to eq("Units params is missing or empty")
+      end
+    end
+
+    context "when given valid units params of empty string" do
+      it "return an object with correct unit_name and multiplication_factor" do
+        get :index, params: { units: "" }
+        json_response = JSON.parse(response.body)
+
+        expect(json_response["error"]).to eq("Units params is missing or empty")
+      end
+    end
+
+  end
 end
